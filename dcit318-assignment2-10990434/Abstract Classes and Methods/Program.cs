36ead8730b04db672c9624fb374b151e845c1993
billibaby <103000682+billibaby@users.noreply.github.com>
2024-06-17 18:00:48 +0000

    Add files via upload

---
+++ dcit318-assignment2-10990434/Abstract Classes and Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OOPAbstractClasses
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(5.0);
+            Shape myRectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine("Circle Area: " + myCircle.GetArea());     // Outputs: Circle Area: 78.53981633974483
+            Console.WriteLine("Rectangle Area: " + myRectangle.GetArea()); // Outputs: Rectangle Area: 24
+        }
+    }
+}
+
---
+++ dcit318-assignment2-10990434/Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOPInterfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            myCar.Move();      // Output: Car is moving
+            myBicycle.Move();  // Output: Bicycle is moving
+        }
+    }
+}
